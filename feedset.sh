650f31179a4694ce088358038f2b51739f4e5bf0
DaiBaojian@CN <32321224+tou-7@users.noreply.github.com>
2022-04-21 14:35:17 +0800

    Create feedset.sh

---
+++ feedset.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -e feeds.conf.default ]
+then
+  sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+  sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+fi  
