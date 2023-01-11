ca97e6767c903220d9e73a3c9980e0dc5f564d7a
DaiBaojian@CN <32321224+tou-7@users.noreply.github.com>
2023-01-11 13:16:59 +0800

    Update feedset.sh

--- feedset.sh
+++ feedset.sh
@@ -2,9 +2,9 @@
 
 if [ -e feeds.conf.default ]
 then
-  git clone https://github.com/kenzok8/small-package package/small-package
-  rm -rf package/small-package/uugamebooster
-  rm -rf package/small-package/luci-app-UUGameAcc
+  #git clone https://github.com/kenzok8/small-package package/small-package
+  #rm -rf package/small-package/uugamebooster
+  #rm -rf package/small-package/luci-app-UUGameAcc
   #rm -rf package/small-package/sing-box
   #rm -rf package/small-package/luci-app-passwall
   #rm -rf package/small-package/luci-app-passwall2
@@ -12,6 +12,6 @@ then
   #git clone https://github.com/xiaorouji/openwrt-passwall/tree/luci package/luci-app-passwall
   #git clone https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
   #sed -i '$a  src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-  #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-  #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+  sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+  sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 fi  
