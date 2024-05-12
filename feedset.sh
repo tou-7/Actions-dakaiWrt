b96664942f00bd81d06a48d5d60564d1fb3e506a
DaiBaojian@CN <32321224+tou-7@users.noreply.github.com>
2024-05-12 23:19:51 +0800

    Update feedset.sh

--- feedset.sh
+++ feedset.sh
@@ -5,4 +5,5 @@ then
   #sed -i '$a  src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
   #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
   #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+  echo ceshi
 fi  
