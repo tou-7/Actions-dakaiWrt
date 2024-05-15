ef691f0ea020d34ff694dc783496a32e3a234d2c
DaiBaojian@CN <32321224+tou-7@users.noreply.github.com>
2024-05-15 13:18:43 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,5 +7,5 @@
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
 #sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
-echo "makeJ1Model=true" >> $GITHUB_ENV
+#echo "makeJ1Model=true" >> $GITHUB_ENV
 
