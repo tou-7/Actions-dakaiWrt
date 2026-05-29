ef532636b791a3baa81eee6decd8bafb65684c28
DaiBaojian@CN <32321224+tou-7@users.noreply.github.com>
2026-05-29 20:35:05 +0800

    Update update_install.sh

--- update_install.sh
+++ update_install.sh
@@ -5,11 +5,9 @@ if grep -E -i "^\s*error\s*:" ../update.log; then
     echo "cancelWorkflow=true" >>$GITHUB_ENV
 else
     #一键命令(防止插件冲突，删除重复)
-    #rm -rf feeds/luci/applications/luci-app-mosdns
-    #rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-    #rm -rf feeds/packages/utils/v2dat
-    #rm -rf feeds/packages/lang/golang
-    #git clone https://github.com/kenzok8/golang -b 1.25 feeds/packages/lang/golang
+    rm -rf feeds/luci/applications/luci-app-mosdns
+    rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns} feeds/packages/utils/v2dat feeds/packages/lang/golang
+    git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
     #===========================================
     ./scripts/feeds install -a 2>&1 | tee ../install.log
     if grep -E -i "^\s*error\s*:" ../install.log; then
