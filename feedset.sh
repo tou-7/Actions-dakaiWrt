42379c98a3c9aacb25c3d28a994e4bb08e82e2ae
DaiBaojian@CN <32321224+tou-7@users.noreply.github.com>
2026-08-21 19:17:01 +0800

    Update feedset.sh

--- feedset.sh
+++ feedset.sh
@@ -7,6 +7,6 @@ then
   #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
   sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
   sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-  sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
+  #sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
   #echo ceshi
 fi  
