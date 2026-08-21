7e62f0c31580023214f80f6aa89cefb96a658c75
DaiBaojian@CN <32321224+tou-7@users.noreply.github.com>
2026-08-21 16:05:15 +0800

    Update update_install.sh

--- update_install.sh
+++ update_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod +x ./scripts/feeds
+chmod -R 777 ./scripts
 ./scripts/feeds update -a 2>&1 | tee ../update.log
 if grep -E -i "^\s*error\s*:" ../update.log; then
     echo "cancelWorkflow=true" >>$GITHUB_ENV
