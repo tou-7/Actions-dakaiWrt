67bee291630f7a8289fafd636c33d1a45dc8bfe7
DaiBaojian@CN <32321224+tou-7@users.noreply.github.com>
2026-09-15 12:34:05 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,4 +8,23 @@
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
 #sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
 #echo "makeJ1Model=true" >> $GITHUB_ENV
+#===============================================
+
+release=24.10
+arch=aarch64_cortex-a53
+
+# OPKG 第三方源
+mkdir -p package/base-files/files/etc/opkg
+
+cat > package/base-files/files/etc/opkg/customfeeds.conf <<EOF
+src/gz passwall_luci https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/passwall_luci
+src/gz passwall_packages https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/passwall_packages
+src/gz passwall2 https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/passwall2
+EOF
+
+# OPKG 公钥
+mkdir -p package/base-files/files/etc/opkg/keys
+
+wget -O package/base-files/files/etc/opkg/keys/0abda65a492b4887 \
+    https://master.dl.sourceforge.net/project/openwrt-passwall-build/ipk.pub
 
