c87aa3dba8da14f00bd2cc6c111422805a45ba53
DaiBaojian@CN <32321224+tou-7@users.noreply.github.com>
2026-09-20 13:04:30 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -10,8 +10,8 @@
 #echo "makeJ1Model=true" >> $GITHUB_ENV
 #===============================================
 
-#release=25.12
-#arch=aarch64_cortex-a53
+release=25.12
+arch=aarch64_cortex-a53
 
 # # OPKG 第三方源
 # mkdir -p package/base-files/files/etc/opkg
@@ -29,37 +29,37 @@
 #     https://master.dl.sourceforge.net/project/openwrt-passwall-build/ipk.pub
 
 #===============================================
-# # apk 第三方源
-# mkdir -p package/base-files/files/etc/apk/repositories.d
+# apk 第三方源
+mkdir -p files/etc/apk/repositories.d
 
-# cat > package/base-files/files/etc/apk/repositories.d/customfeeds.list <<EOF
-# https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/passwall_luci/packages.adb
-# https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/passwall_packages/packages.adb
-# https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/passwall2/packages.adb
-# EOF
+cat > files/etc/apk/repositories.d/customfeeds.list <<EOF
+https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/passwall_luci/packages.adb
+https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/passwall_packages/packages.adb
+https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/passwall2/packages.adb
+EOF
 
-# cat package/base-files/files/etc/apk/repositories.d/customfeeds.list
+cat files/etc/apk/repositories.d/customfeeds.list
 
-# # apk 公钥
-# mkdir -p package/base-files/files/etc/apk/keys
+# apk 公钥
+mkdir -p files/etc/apk/keys
 
-# wget -O package/base-files/files/etc/apk/keys/openwrt-passwall-build.pem \
-#     https://master.dl.sourceforge.net/project/openwrt-passwall-build/apk.pub
+wget -O files/etc/apk/keys/openwrt-passwall-build.pem \
+    https://master.dl.sourceforge.net/project/openwrt-passwall-build/apk.pub
 
-# cat package/base-files/files/etc/apk/keys/openwrt-passwall-build.pem
+cat files/etc/apk/keys/openwrt-passwall-build.pem
 
 #=======================================================================
-mkdir -p package/base-files/files/etc/uci-defaults
+# mkdir -p package/base-files/files/etc/uci-defaults
 
-chmod -R 777 package/base-files/
+# chmod -R 777 package/base-files/
 
-cp ../99-my-init package/base-files/files/etc/uci-defaults/99-my-init
+# cp ../99-my-init package/base-files/files/etc/uci-defaults/99-my-init
 
-chmod +x package/base-files/files/etc/uci-defaults/99-my-init 
+# chmod +x package/base-files/files/etc/uci-defaults/99-my-init 
 
-cat package/base-files/files/etc/uci-defaults/99-my-init
+# cat package/base-files/files/etc/uci-defaults/99-my-init
 
-make package/base-files/clean
-make package/base-files/compile V=s
+# make package/base-files/clean
+# make package/base-files/compile V=s
 
     
